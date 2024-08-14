9515482f674d50879ad567a96d1c1c39411b2384
DeeprajDas <mail30deeprajdas@gmail.com>
2024-08-14 22:36:09 +0530

    subscription section done

--- hiremystudios_project_1/script.js
+++ hiremystudios_project_1/script.js
@@ -94,27 +94,23 @@ productsButtonElement.addEventListener("click", (event) => {
   );
   productsButtonElement.style.backgroundColor =
     productsButtonBackgroundList[getNextProductsButtonBackgroundListIndex()];
-  // Start the rotation animation
   productsImageElement.style.animation = "none";
-  // Force a reflow to ensure the animation is reset
-  productsImageElement.offsetHeight; // Trigger a reflow
+  productsImageElement.offsetHeight;
   productsImageElement.style.animation = `productsRotateOnChange 1s forwards`;
   productsContainerElement.offsetHeight;
   productsContainerElement.style.background =
     productsContainerGradientList[getNextProductsGradientIndex()];
-  // Change the image after the first rotation completes (5 seconds)
   productsImageElement.setAttribute(
     "src",
     productsShowCase[currentCounterProductsShow]["linkSrc"]
   );
-  // Update alt text and description after the reverse animation completes (another 5 seconds)
 
   productsImageElement.setAttribute(
     "alt",
     productsShowCase[currentCounterProductsShow]["name"]
   );
   productsContentElement.classList.remove("text-focus-in");
-  productsContentElement.offsetHeight; // Trigger a reflow
+  productsContentElement.offsetHeight;
   productsContentElement.classList.add("text-focus-in");
   productsContentElement.textContent =
     productsShowCase[currentCounterProductsShow]["description"];
@@ -128,22 +124,18 @@ document.addEventListener("DOMContentLoaded", () => {
   ];
 
   const backgroundVideoElement = document.querySelector("#background-video");
-  const videoSourceElement = backgroundVideoElement.querySelector("source"); // Ensure this is defined
 
   let videoIndex = 0;
-  const fadeDuration = 0.5; // Duration of the fade animation in seconds
-  const fadeBeforeEnd = 5; // Number of seconds before the end to start fading out
+  const fadeDuration = 0.5;
 
-  // Function to set the video source based on index
   const setVideoSource = (index) => {
-    videoSourceElement.src = productBackgroundVideoSrc[index];
+    backgroundVideoElement.src = productBackgroundVideoSrc[index];
     backgroundVideoElement.load();
     backgroundVideoElement.play();
   };
 
   backgroundVideoElement.classList.add("zoom-animation");
 
-  // Set the initial video source to the video at index 0
   setVideoSource(videoIndex);
 
   const getNextVideoIndex = () => {
@@ -160,47 +152,28 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   backgroundVideoElement.addEventListener("ended", () => {
-    // Apply fade-out animation
     fadeOut();
 
-    // Determine the next video source index
     const nextIndex = getNextVideoIndex();
 
-    // Update the source element
     const nextSource = productBackgroundVideoSrc[nextIndex];
-    videoSourceElement.src = nextSource;
+    backgroundVideoElement.src = nextSource;
 
-    // Wait for the fade-out animation to complete
     setTimeout(() => {
-      // Reload and play the new video
       backgroundVideoElement.load();
       backgroundVideoElement.play();
 
-      // Apply fade-in animation
       fadeIn();
-    }, fadeDuration * 1000); // Convert duration to milliseconds
+    }, fadeDuration * 1000);
   });
 });
 
-// Subscription Section
 const weeklyUpdatesList = [
   {
-    updateDate: "August 14, 2024",
-    updateHeading: "Harvesting Begins for the Cashew Season!",
+    updateDate: "September 11, 2024",
+    updateHeading: "Preparing for Upcoming Nut Festival",
     updateContent:
-      "We’re excited to announce the start of our cashew harvest this week at Aura Farming. The trees are yielding a bountiful crop of high-quality cashews, and our team is busy hand-harvesting to ensure the best selection. Stay tuned for updates on the processing and quality checks!",
-  },
-  {
-    updateDate: "August 21, 2024",
-    updateHeading: "New Pistachio Varieties in the Orchard",
-    updateContent:
-      "This week, we planted new varieties of pistachios in our orchard. These varieties are known for their rich flavor and superior texture. We’re looking forward to watching them grow and will keep you updated on their progress. Our current pistachio crop continues to thrive, with excellent quality nuts being harvested.",
-  },
-  {
-    updateDate: "August 28, 2024",
-    updateHeading: "Hazelnut Processing and Quality Control",
-    updateContent:
-      "Our hazelnuts are now in the processing phase. We’ve implemented enhanced quality control measures to ensure each nut meets our high standards. After processing, the hazelnuts will be ready for packaging and distribution. We’re committed to delivering the freshest and most flavorful hazelnuts to our customers.",
+      "We’re thrilled to announce our participation in the upcoming Nut Festival! Our booth will feature our finest cashews, pistachios, and hazelnuts. It’s a great opportunity for us to connect with our community and showcase the premium quality of our products. We hope to see you there!",
   },
   {
     updateDate: "September 4, 2024",
@@ -209,10 +182,22 @@ const weeklyUpdatesList = [
       "At Aura Farming, sustainability is a core value. This week, we’ve introduced new water-saving irrigation techniques and are using organic fertilizers to reduce our environmental impact. Our commitment to sustainable practices ensures that our nuts are not only high-quality but also grown in an eco-friendly manner.",
   },
   {
-    updateDate: "September 11, 2024",
-    updateHeading: "Preparing for Upcoming Nut Festival",
+    updateDate: "August 28, 2024",
+    updateHeading: "Hazelnut Processing and Quality Control",
     updateContent:
-      "We’re thrilled to announce our participation in the upcoming Nut Festival! Our booth will feature our finest cashews, pistachios, and hazelnuts. It’s a great opportunity for us to connect with our community and showcase the premium quality of our products. We hope to see you there!",
+      "Our hazelnuts are now in the processing phase. We’ve implemented enhanced quality control measures to ensure each nut meets our high standards. After processing, the hazelnuts will be ready for packaging and distribution. We’re committed to delivering the freshest and most flavorful hazelnuts to our customers.",
+  },
+  {
+    updateDate: "August 21, 2024",
+    updateHeading: "New Pistachio Varieties in the Orchard",
+    updateContent:
+      "This week, we planted new varieties of pistachios in our orchard. These varieties are known for their rich flavor and superior texture. We’re looking forward to watching them grow and will keep you updated on their progress. Our current pistachio crop continues to thrive, with excellent quality nuts being harvested.",
+  },
+  {
+    updateDate: "August 14, 2024",
+    updateHeading: "Harvesting Begins for the Cashew Season!",
+    updateContent:
+      "We’re excited to announce the start of our cashew harvest this week at Aura Farming. The trees are yielding a bountiful crop of high-quality cashews, and our team is busy hand-harvesting to ensure the best selection. Stay tuned for updates on the processing and quality checks!",
   },
 ];
 
@@ -228,4 +213,39 @@ for (i of weeklyUpdatesList) {
       <p class="weekly-update-element-content">${i["updateContent"]}</p>
     </div>
   `;
+}
+
+document
+  .getElementById("form-submit-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    // Get reCAPTCHA response
+    const recaptchaResponse = grecaptcha.getResponse();
+
+    if (recaptchaResponse.length === 0) {
+      document.getElementById("error-message").style.display = "block";
+      document.getElementById("error-message").textContent =
+        "Please complete the reCAPTCHA.";
+      return;
+    }
+    const email = document.getElementById("form-email").value.trim();
+    const subject = document.getElementById("form-subject").value.trim();
+    const message = document.getElementById("form-message").value.trim();
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.textContent = "";
+
+    if (!validateEmail(email)) {
+      errorMessage.textContent = "Please enter a valid email address.";
+    } else if (subject === "") {
+      errorMessage.textContent = "Please enter a subject.";
+    } else if (message === "") {
+      errorMessage.textContent = "Please enter a message.";
+    } else {
+      errorMessage.textContent = "Form submitted successfully!";
+    }
+  });
+
+function validateEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
 }
